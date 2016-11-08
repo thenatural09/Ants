1f96f34ac8831033912e2477536ed71b607d12a2
Troy <tward4@tulane.edu>
2016-11-08 10:19:21 -0500

    Adding ant animation and fps

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,13 +1,20 @@
 package com.theironyard;
 
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 
 public class Main extends Application {
@@ -25,14 +32,60 @@ public class Main extends Application {
         return ants;
     }
 
+    void drawAnts(GraphicsContext context) {
+        context.clearRect(0,0,WIDTH,HEIGHT);
+        for (Ant ant : ants) {
+            context.setFill(Color.BLACK);
+            context.fillOval(ant.x,ant.y,5,5);
+        }
+    }
+
+    static double randomStep() {
+        return Math.random() * 2 - 1;
+    }
+
+    Ant moveAnt(Ant ant) {
+        ant.x += randomStep();
+        ant.y += randomStep();
+        return ant;
+    }
+
+    void moveAnts() {
+        ants = ants.stream()
+                .map(this::moveAnt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    long lastTimestamp = 0;
+    int fps(long currentTimestamp) {
+        double diff = currentTimestamp - lastTimestamp;
+        diff = diff / 1000000000;
+        return (int) (1 / diff);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
         primaryStage.setTitle("Ants");
-        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        primaryStage.setScene(scene);
         primaryStage.show();
-    }
 
+        Canvas canvas = (Canvas) scene.lookup("#canvas");
+        Label fpsLabel = (Label) scene.lookup("#fps");
+        GraphicsContext context = canvas.getGraphicsContext2D();
+        ants = createAnts();
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                moveAnts();
+                drawAnts(context);
+                fpsLabel.setText(String.valueOf(fps(now)));
+                lastTimestamp = now;
+            }
+        };
+        timer.start();
+    }
 
     public static void main(String[] args) {
         launch(args);
